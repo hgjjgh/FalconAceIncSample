d1f473e4cbc87254e7e86c84a985366577b593ab
hgjjgh <z7539518426q@gmail.com>
2022-08-26 17:56:04 +0800

    fix UI錯誤, 事後檢討更正項目: 1. 修正跑版UI 2. 修正日期格式錯誤

--- app/src/main/java/com/letsgo/myapplicationtest/ui/Base/BaseBind.kt
+++ app/src/main/java/com/letsgo/myapplicationtest/ui/Base/BaseBind.kt
@@ -5,23 +5,29 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.letsgo.myapplicationtest.R
 import java.text.SimpleDateFormat
 import java.util.*
 
 @BindingAdapter("app:glideBackground")
 fun glideBackground(imageView: ImageView, url: String?) {
     url?.run {
-        Glide.with(imageView.context).load(url).into(imageView)
+        Glide.with(imageView.context)
+            .load(url)
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .error(R.drawable.ic_launcher_foreground)
+            .into(imageView)
     }
 }
 
 @BindingAdapter("app:timeFormat")
 fun timeFormat(textView: TextView, timeFormat: Long?) {
     if (timeFormat == null) {
-        textView.visibility =  View.GONE
+        textView.visibility = View.GONE
     } else {
-        val sdf = SimpleDateFormat("MM dd, yyyy HH:mm aa", Locale.ENGLISH)
-        val str = sdf.format(timeFormat *1000)
+        textView.visibility = View.VISIBLE
+        val sdf = SimpleDateFormat("MMM dd, yyyy 'at' HH:mm aa", Locale.ENGLISH)
+        val str = sdf.format(timeFormat * 1000)
         textView.text = str
     }
 }
