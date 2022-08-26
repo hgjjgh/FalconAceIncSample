fa6d8f72271a6a453990652701f5637d71297ecd
hgjjgh <z7539518426q@gmail.com>
2022-08-26 19:42:34 +0800

    fix 修正response 篩選邏輯, 事後檢討更正項目: 新增該抬頭無子項目時刪除該抬頭邏輯

--- app/src/main/java/com/letsgo/myapplicationtest/network/repository/NewsRepository.kt
+++ app/src/main/java/com/letsgo/myapplicationtest/network/repository/NewsRepository.kt
@@ -4,7 +4,8 @@ import com.letsgo.myapplicationtest.common.CommonConfig
 import com.letsgo.myapplicationtest.network.service.ApiClient
 import com.letsgo.myapplicationtest.network.service.NewsService
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class NewsRepository {
     private val newsService: NewsService by lazy {
@@ -13,10 +14,30 @@ class NewsRepository {
 
     suspend fun fetchNewsList() = flow {
         val res = newsService.fetchNewsList()
-        val data = res?.getVector?.items?.filter {
+
+        val typeData = res?.getVector?.items?.filter {
             it.type == CommonConfig.NEWS_TYPE_DRIVER ||
                     it.type == CommonConfig.NEWS_TYPE_NEWS
         }
-        emit(data)
+
+        val result = typeData?.filterNot { item ->
+            var saveData = false
+
+            if (item.type == CommonConfig.NEWS_TYPE_DRIVER) {
+                for (i in typeData.indices) {
+                    val bean = typeData[i]
+
+                    if (bean.type == CommonConfig.NEWS_TYPE_NEWS &&
+                        item.meta?.section == bean.meta?.section) {
+                        saveData = true
+                        break
+                    }
+                }
+            }
+
+            item.type == CommonConfig.NEWS_TYPE_DRIVER && !saveData
+        }
+
+        emit(result)
     }.flowOn(Dispatchers.IO)
 }
